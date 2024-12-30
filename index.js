4e5159fd14da6135de74000861756a68d95bb23c
TerrenceArceo <76758014+TerrenceArceo@users.noreply.github.com>
2024-12-30 10:28:17 -0800

    display flex version of the desktop layout

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const main = document.getElementById("main")
+const dessertList = document.getElementById("dessert-list")
+const cart = document.getElementById("cart")
 let dessertData =
 
 fetch('../data.json')
@@ -6,16 +7,40 @@ fetch('../data.json')
     .then(data => {
         dessertData = data
         let htmlContent = ''
-        
-        for (dessert of dessertData) {
+
+        dessertData.forEach((dessert, index) => {
+            let dessertPrice = dessert.price
             htmlContent += `
-                <img src="${dessert.image.mobile}">
-                <p>${dessert.category}</p>
-                <h2>${dessert.name}</h2>
-                <p>${dessert.price}</p>
+            <div class="dessert-item">
+                <picture>
+                    <source media="(min-width: 900px)" srcset="${dessert.image.desktop}">
+                    <source media="(min-width: 712px)" srcset="${dessert.image.tablet}">
+                    <img src="${dessert.image.mobile}" alt="IfItDoesntMatchAnyMedia" class="main-img">
+                </picture>
+                <button class="btn add-btn" id="${index}">
+                    <img src="../assets/images/icon-add-to-cart.svg" class="add-to-cart-icon">
+                    Add to Cart
+                </button>
+                <p class="category">${dessert.category}</p>
+                <h2 class="name">${dessert.name}</h2>
+                <p class="price">$${dessertPrice.toFixed(2)}</p>
+            </div>
             `
-        }
+        }) 
 
-        main.innerHTML = htmlContent
-
+        dessertList.innerHTML = htmlContent
     })
+
+const cartHtmlContent = () => {
+    cart.innerHTML = `
+        <div class="cart-container">
+            <h2 class="cart-title">Your Cart (0)</h2>
+            <div class="cart-content">
+                <img class="empty-cart-cake" src="../assets/images/illustration-empty-cart.svg" alt="a picture of cake illustrating that a cart is empty">
+                <p class="empty-cart-message">Your added items will appear here</p>
+            </div>
+        </div>
+    `
+}
+
+cartHtmlContent()
